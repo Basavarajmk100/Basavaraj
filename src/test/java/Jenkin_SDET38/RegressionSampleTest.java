de31f4f2259b502d11884617c031e5aeb397c78d
Admin <Admin@DESKTOP-GADFEBD>
2022-12-05 16:45:25 +0530

    suite

---
+++ src/test/java/Jenkin_SDET38/RegressionSampleTest.java
@@ -0,0 +1,17 @@
+package Jenkin_SDET38;
+
+import org.testng.annotations.Test;
+
+public class RegressionSampleTest 
+{
+	@Test
+	public void RegressionSample()
+	{
+		System.out.println("Welcome to India");
+		
+	}
+	
+	
+	
+
+}
---
+++ src/test/java/Jenkin_SDET38/SmokeSampleTest.java
@@ -0,0 +1,13 @@
+package Jenkin_SDET38;
+
+import org.testng.annotations.Test;
+
+public class SmokeSampleTest {
+	@Test
+	public void SmokeSample()
+	{
+		System.out.println("Welcome to India");
+		
+	}
+
+}
